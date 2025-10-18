4fa613168c99cf3bce5b19fb5a2e7ee522ea8b13
ronilchanchlani <ronil.chanchlani.medschool@gmail.com>
2025-10-18 22:09:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import About from './pages/About';
-import Questions from './pages/Questions';
-import Resources from '../../src/pages/Resources';
-import Contact from './pages/Contact';
-import './App.css';
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Navbar /> {/* Navbar now always shows */}
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  )
 }
 
-export default App;
+export default App
